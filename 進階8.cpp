4fcd6f606e77d89b98570d3143fff41bb33c0d92
yuqing0605 <48577102+yuqing0605@users.noreply.github.com>
2019-04-26 11:01:59 +0800

    Add files via upload

---
+++ 進階8.cpp
@@ -0,0 +1,46 @@
+#include<iostream> 
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+{
+    int N;
+    int T;
+    int n, t, m; 
+    int money[105];
+    int i;	
+	printf("請輸入有幾個顧客："); 
+    scanf("%d",&N);
+    printf("請輸入有幾筆交易："); 
+    scanf("%d",&T);
+    i=0;
+    
+    while( i<N )
+    {money[i] = 0; 
+    i = i+1;}
+
+    printf("請輸入有幾個顧客，存款還是提款，存款或提款了m元：\n"); 
+    i = 0;
+
+    while( i<T ){
+    scanf("%d%d%d",&n,&t,&m); 
+
+    if( t==1 )
+    {money[n] = money[n] + m;}
+    
+    if( t==2 )
+    {money[n] = money[n] - m;}
+    
+    i = i+1;}
+    
+    printf("每名顧客的存款如下：\n"); 
+    i = 0;
+
+    while( i<N )
+    {	printf("第%d名顧客：剩餘存款為%d元\n",i+1,money[i]); 
+        i = i+1;}
+        
+	system("PAUSE");
+    return 0;  
+}
